4f5675ce8eaa7910ebd1d5edfadbcba07138f8af
Keony Willian Schröer <keony@edu.univali.br>
2019-10-29 00:58:32 -0300

    Styles and call to api configured

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
+
+export default function Cart() {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th>#</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://imgcentauro-a.akamaihd.net/900x900/92817619/tenis-oxer-velocity-masculino-img.jpg"
+                alt="tênis"
+              />
+            </td>
+            <td>
+              <strong>Tẽnis</strong>
+              <span>129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 258,00</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>Total</span>
+          <strong>R$ 1920,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
+}
